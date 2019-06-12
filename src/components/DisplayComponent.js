c9a4d5a49d0fa74588a0c532afa766b8007e6d11
Stipan Hržić <stipan.hrzic@gmail.com>
2019-06-12 11:00:05 +0200

    All  ''

--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -16,14 +16,14 @@ class DisplayComponent extends React.Component{
 
     getColors = async() => {
         try{
-            const response = await fetch("http://www.colr.org/json/color/random");
+            const response = await fetch('http://www.colr.org/json/color/random');
             const colorData = await response.json();
             const color = colorData.colors[0].hex;
             const newColors = [...this.state.colors, color];
             this.setState({colors:newColors});
         }
         catch(err){
-            console.log("Error: ", err);
+            console.log('Error: ', err);
         }
     }
     
@@ -50,9 +50,9 @@ class DisplayComponent extends React.Component{
                     onClick={this.handleClickColor}
                     className ='display-box'
                     style = {{
-                            background: this.state.colorValue ? `${this.state.colorValue}` : "black",
+                            background: this.state.colorValue ? `${this.state.colorValue}` : 'black',
                         }}>
-                    <p  className = "display-text"
+                    <p  className = 'display-text'
                         style = {{ display: this.state.textValue? 'block' : 'none'
                         }}>
                             {this.state.textValue}
@@ -61,7 +61,7 @@ class DisplayComponent extends React.Component{
                 <input  className='input-text'
                         type='text'
                         placeholder = 'start typing (max. 120)'
-                        maxLength="120"
+                        maxLength='120'
                         value = {this.state.textValue} 
                         onChange = {this.handleInputChange}/>
             </div>
